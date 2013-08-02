ad32e2b920c4cd9ecb40c63949df3a7a02eb296a
Alexander Kachkaev <alexander@kachkaev.ru>
2013-08-02 11:43:33 +0100

    Implement UpdateCommandAlias.php to be inherited by commands in custom namespaces

---
+++ Command/Datasets/Components/Attributes/UpdateCommandAlias.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Kachkaev\PostgresHelperBundle\Command\Datasets\Components\Attributes;
+
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Kachkaev\PostgresHelperBundle\Command\AbstractParameterAwareCommand;
+
+abstract class UpdateCommandAlias extends AbstractParameterAwareCommand
+{
+    
+    private $commandName;
+    private $datasetSchemaName;
+    private $componentName;
+    private $attributesToUpdate;
+    
+    public function __construct($name = null)
+    {
+        $config = $this->preconfigure();
+        $this->commandName = $config['command-name'];
+        $this->datasetSchemaName = $config['dataset-schema'];
+        $this->componentName = $config['component-name'];
+        $this->attributesToUpdate = $config['attributes-to-update'];
+
+        parent::__construct($name);
+    }
+    
+    /**
+     * @return array(
+     *     'command-name' => 'namespace:command',
+     *     'command-description' => 'Updates xxx in yyy', // optional
+     *     'dataset-namespace' => 'my_namespace',
+     *     'component-name' => 'my_component',
+     *     'attributes-to-update' => array('a1', 'a2', 'aN'),
+     * )
+     */
+    abstract protected function preconfigure();
+    
+    protected function configure()
+    {
+        $this
+            ->setName($this->commandName)
+            ->setDescription(sprintf('Updates %s in component %s', implode(', ', $this->attributesToUpdate), $this->componentName))
+            ->makeDatasetAware($this->datasetSchemaName)
+            ->addOption('filter', null, InputOption::VALUE_REQUIRED,
+                    'sql WHERE to filter records and get their ids',
+                    null)
+            ->addOption('chunk-size', null, InputOption::VALUE_REQUIRED,
+                    sprintf('number of records in a batch'),
+                    null)
+            ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->processInput($input, $output, $extractedArguments);
+        
+
+        $datasetManager = $this->getDatasetManager($extractedArguments['dataset-schema']);
+        $dataset = $datasetManager->get($extractedArguments['dataset-name']);
+        
+        $filter = $input->getOption('filter') ?: 'TRUE';
+        
+        $invokedCommandName = 'ph:datasets:components:attributes:update';
+        $invokedCommand = $this->getApplication()->find($invokedCommandName);
+        $invokedCommandArguments = [
+                'command' => $invokedCommandName,
+                'dataset-full-name' => $dataset->getFullName(),
+                'component-name' => $this->componentName,
+                'attribute-names' => implode(',', $this->attributesToUpdate),
+                '--filter' => $filter,
+        ];
+        
+        $chunkSize = $input->getOption('chunk-size');
+        if ($chunkSize) {
+            $invokedCommandArguments['--chunk-size'] = $chunkSize;
+        }
+        
+        $invokedCommandInput = new ArrayInput($invokedCommandArguments);
+        return $invokedCommand->run($invokedCommandInput, $output);
+    }
+}
