5ece97dce97f09cf17f34fc8921edb753e0091e7
Alexander Kachkaev <alexander@kachkaev.ru>
2013-08-13 11:56:27 +0100

    ph:datasets:components:records:copy command now supports more sophisticated attribute mappings

--- Command/Datasets/Components/Records/CopyCommand.php
+++ Command/Datasets/Components/Records/CopyCommand.php
@@ -28,7 +28,7 @@ class CopyCommand extends AbstractParameterAwareCommand
             ->addOption('existing-only', null, InputOption::VALUE_NONE, 'Only update attribute values of the records that already exist in the destination dataset component')
             ->addOption('missing-only', null, InputOption::VALUE_NONE, 'Only insert records that don’t exist in the destination dataset component')
             ->addOption('ignore-attribute-mismatch', null, InputOption::VALUE_NONE, 'Does not throw an error when there are mismatches in attributes (columns) between the datasets')
-            ->addOption('attribute-mappings', null, InputOption::VALUE_REQUIRED, 'Comma-separated array of attribute (column) names that need to be renamed / casted, e.g. "myfield->myfield_with_new_name,myotherfield::int->myotherfield_of_new_type"')
+            ->addOption('attribute-mappings', null, InputOption::VALUE_REQUIRED, 'Comma-separated array of attribute (column) names that need to be renamed / casted, e.g. "myfield->myfield_with_new_name;myotherfield::int->myotherfield_of_new_type;mythirdfield|convert(mythirdfield)->mythirdfield_of_new_type"')
             ;
     }
 
@@ -56,12 +56,16 @@ class CopyCommand extends AbstractParameterAwareCommand
         
         $attributeMappings = [];
         if ($attributeMappingsAsStr) {
-            $attributeMappingsAsRawArray = explode(',', $attributeMappingsAsStr);
+            $attributeMappingsAsRawArray = explode(';', $attributeMappingsAsStr);
             foreach($attributeMappingsAsRawArray as $am) {
-                if (!preg_match('/^\s*([a-z_0-9\:\s]+)\s*->\s*([a-z_0-9]+)\s*$/i', $am, $matches)) {
-                    throw new \InvalidArgumentException(sprintf('Could not parse attribute-mappings option, please check its format'));
+                if (!preg_match('/^\s*([a-z_0-9\:\s]+)\s*(\|[a-zA-Z0-9_\s\[\]\(\),]+)?\s*->\s*([a-z_0-9]+)\s*$/i', $am, $matches)) {
+                    throw new \InvalidArgumentException(sprintf('Could not parse attribute-mappings option, please check its format %s', $am));
                 }
-                $attributeMappings[$matches[1]] = $matches[2];
+                if ($matches[2]) {
+                    $attributeMappings[$matches[1] . $matches[2]] = $matches[3];
+                } else {
+                    $attributeMappings[$matches[1]] = $matches[3];
+                }
             }
         }
 
