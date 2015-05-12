b108f3cf5a39aae248baf93bfc73615f27e00e1f
Alexander Kachkaev <alexander@kachkaev.ru>
2015-05-12 13:32:02 +0100

    Change template path i the SchemaMangaer + remove spaces at eols

--- Model/Schema/SchemaManager.php
+++ Model/Schema/SchemaManager.php
@@ -14,13 +14,13 @@ use Kachkaev\DAFBundle\Model\TemplateManaging\SQLTemplateManager;
 
 /**
  * @author  "Alexander Kachkaev <alexander@kachkaev.ru>"
- * 
+ *
  * @DI\Service("daf.schema_manager")
  */
 
 class SchemaManager implements ManagerInterface
 {
-    
+
     protected $systemSchemas = [
             "pg_toast",
             "pg_temp_1",
@@ -28,7 +28,7 @@ class SchemaManager implements ManagerInterface
             "pg_catalog",
             "information_schema",
         ];
-    
+
     /**
      *  @var ContainerInterface */
     protected $container;
@@ -36,12 +36,12 @@ class SchemaManager implements ManagerInterface
     /**
      *  @var SQLTemplateManager */
     protected $sqlTemplateManager;
-    
+
     /**
      * @var SchemaNameValidator
      */
     protected $nameValidator;
-    
+
     /**
      * @DI\InjectParams({
      *     "container" = @DI\Inject("service_container"),
@@ -54,45 +54,45 @@ class SchemaManager implements ManagerInterface
         //$this->nameValidator = $this->getValidator('schema_name');
         $this->updateList();
     }
-    
+
     public function updateList()
     {
         // Method is empty because listNames is not using caching
     }
-    
+
     public function listNames()
     {
         $allSchemas =  $this->sqlTemplateManager->runAndFetchAllAsList("dataset_abstraction#schemas/list");
         $filteredSchemas = array_diff($allSchemas, $this->systemSchemas);
-        
+
         return $filteredSchemas;
     }
-    
+
     public function has($schemaName)
     {
         $schemas = $this->listNames();
 
         return in_array($schemaName, $schemas);
     }
-    
+
     public function init($schemaName)
     {
         $this->sqlTemplateManager->run("dataset_abstraction#schemas/init", [
                 'schema' => $schemaName
-            ]);    
+            ]);
     }
 
     public function delete($schemaName)
     {
         if (in_array($schemaName, $this->systemSchemas)) {
-            throw new \InvalidArgumentException("You are not allowed to delete system schema $schemaName");    
+            throw new \InvalidArgumentException("You are not allowed to delete system schema $schemaName");
         }
-        
+
         $this->sqlTemplateManager->run("dataset_abstraction#schemas/delete", [
                 'schema' => $schemaName
             ]);
     }
-    
+
     public function get($schemaName)
     {
         throw new \LogicException('As schemas are not objects, it is not possible to extract an instance of it');
@@ -103,11 +103,11 @@ class SchemaManager implements ManagerInterface
     {
         throw new \LogicException('Schemas cannot be renamed');
     }
-    
+
     /**
      * Updates all schema functions
      * Function templates are stored in *Bundle/Resources/views/pgsql/schema/functions
-     * 
+     *
      * There are 3 categories of functions:
      * 1) Standard - created only once in the schema
      *       myfunction.pgsql.twig
@@ -117,21 +117,21 @@ class SchemaManager implements ManagerInterface
      *
      * 3) Related to datasets and dataset types - same as 2, but is applicable only to datasets having a certain type
      *       dataset__{my_function}.{dataset_type}.pgsql.twig
-     *       
+     *
      * 4) Related to datasets and specific to instances of components (for components with multiple instances)
      *      dataset__{component_family_name}__instance__{my_function}.pgsql.twig
-     *      
+     *
      * 5) [stub] Related to datasets, specific to instances of components and type
      *      dataset__{component_family_name}__instance__{my_function}.{dataset_type}.pgsql.twig
      *
      */
     public function updateFunctions($schemaName)
     {
-        $directory = sprintf('%s/../src/Kachkaev/PR%sBundle/Resources/views/pgsql/schema/functions', $this->container->getParameter('kernel.root_dir'), ucfirst($schemaName));
+        $directory = $this->container->getParameter('daf.query_templates_namespace_lookups')[$schemaName]['path'] . '/pgsql/schema/functions';
         if (!is_dir($directory)) {
             return;
         }
-        
+
         // Find all pgsql templates in the directory
         $finder = new Finder();
         $finder
@@ -139,9 +139,9 @@ class SchemaManager implements ManagerInterface
         ->in($directory)
         ->name('*.pgsql.twig')
         ->depth(0);
-    
+
         $functionsByCategory = [[], [], [], []];
-    
+
         // Categorise found templates
         foreach ($finder as $file) {
             $functionName = $file->getBaseName('.pgsql.twig');
@@ -158,27 +158,27 @@ class SchemaManager implements ManagerInterface
                 throw new \LogicExcepton(sprintf("Cannot determine type of function template %s", $functionName));
             }
         }
-    
+
         // Delete all functions
         $this->sqlTemplateManager->run('dataset_abstraction#schemas/delete-all-functions', [
                 'schema' => $schemaName,
                 ]);
-    
+
         // Add standard functions (category 1)
         foreach ($functionsByCategory[0] as $function) {
             $this->sqlTemplateManager->run($schemaName.'#schema/functions/'.$function);
         };
-    
+
         // Get corresponding DatasetManager
         $serviceName = sprintf('daf.dataset_manager.%s', $schemaName);
         if ($this->container->has($serviceName)) {
             $datasetManager = $this->container->get($serviceName);
-            
+
             // Add dataset-related functions (cateogries 2 and 3)
             foreach ($datasetManager->listNames() as $datasetName) {
                 $dataset = $datasetManager->get($datasetName);
                 $datasetType = $dataset->getProperty('type');
-        
+
                 // Loop through functions of cateogry 2 and apply them or add corresponding functions from category 3
                 foreach ($functionsByCategory[1] as $function) {
                     $typeSpecificFunction = $function.'.'.$datasetType;
@@ -194,7 +194,7 @@ class SchemaManager implements ManagerInterface
                                 ]);
                     }
                 }
-        
+
                 // Add functions from category 3 that do not have corresponding functions in category 2
                 foreach ($functionsByCategory[2] as $typeSpecificFunction) {
                     list($function, $type) = explode('.', $typeSpecificFunction);
@@ -204,8 +204,8 @@ class SchemaManager implements ManagerInterface
                             ]);
                     }
                 }
-                
-                // Add functions from category 4 
+
+                // Add functions from category 4
                 foreach ($functionsByCategory[3] as $componentInstanceSpecificFunction) {
                     list($nothing1, $familyName, $nothing2, $functionName) = explode('__', $componentInstanceSpecificFunction);
                     $instanceNames = $dataset->getComponentManager()->listInstanceNames($familyName);
@@ -219,18 +219,18 @@ class SchemaManager implements ManagerInterface
             }
         }
     }
-    
+
     /**
      * Updates all schema types
      * Type templates are stored in *Bundle/Resources/views/pgsql/schema/types
      */
     public function updateTypes($schemaName)
     {
-        $directory = sprintf('%s/../src/Kachkaev/PR%sBundle/Resources/views/pgsql/schema/types', $this->container->getParameter('kernel.root_dir'), ucfirst($schemaName));
+        $directory = $this->container->getParameter('daf.query_templates_namespace_lookups')[$schemaName]['path'] . '/pgsql/schema/functions';
         if (!is_dir($directory)) {
             return;
         }
-        
+
         // Find all pgsql templates in the directory
         $finder = new Finder();
         $finder
@@ -238,7 +238,7 @@ class SchemaManager implements ManagerInterface
         ->in($directory)
         ->name('*.pgsql.twig')
         ->depth(0);
-    
+
         // Execute all templates
         foreach ($finder as $file) {
             $templateName = $file->getBaseName('.pgsql.twig');
